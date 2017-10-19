473b13e5f6160295e84bd076778238458a78506e
yedim <yelimi0416@gmail.com>
2017-10-19 21:03:38 +0900

    #2 Player가 다중점프 제한

--- Running Mirim/Assets/Scripts/PlayerController.cs
+++ Running Mirim/Assets/Scripts/PlayerController.cs
@@ -9,18 +9,29 @@ public class PlayerController : MonoBehaviour {
 
     private Rigidbody2D myRigidbody;
 
+    public bool grounded; //checkbox
+    public LayerMask whatIsGround; // list
+
+    private Collider2D myCollider;
+
 	// Use this for initialization
 	void Start () {
         myRigidbody = GetComponent<Rigidbody2D>();
-	}
+        myCollider = GetComponent<Collider2D>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
+
+        grounded = Physics2D.IsTouchingLayers(myCollider, whatIsGround);
         myRigidbody.velocity = new Vector2(moveSpeed,myRigidbody.velocity.y);// Player을 moveSpeed만큼 앞으로 이동. Vector2는 x,y좌표 저장. 
            
         if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) //스페이스바나 마우스 눌렸을 때
         {
-            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpForce);
+            if(grounded)
+            {
+                myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpForce);
+            }
         }
 	}
 }
