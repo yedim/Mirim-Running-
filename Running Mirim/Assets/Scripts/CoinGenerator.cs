d12eb95b2f1799c0b74865051c59685d923c3a73
yedim <yelimi0416@gmail.com>
2017-10-22 15:04:23 +0900

    #16 Restart할 때 Jelly가 지워지지 않고 계속 생성되는 오류

--- Running Mirim/Assets/Scripts/CoinGenerator.cs
+++ Running Mirim/Assets/Scripts/CoinGenerator.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class CoinGenerator : MonoBehaviour {
 
+    //젤리 생성을 위한 부모 오브젝트
+    public GameObject CoinParent;
     public ObjectPooler coinPool;
 
     public float distanceBetweenCoins;
@@ -21,5 +23,19 @@ public class CoinGenerator : MonoBehaviour {
         GameObject coin3 = coinPool.GetPooledObject();
         coin3.transform.position = new Vector3(startPosition.x + distanceBetweenCoins, startPosition.y, startPosition.z);
         coin3.SetActive(true);
+
+        coin1.transform.parent = CoinParent.transform; //부모로 지정
+        coin2.transform.parent = CoinParent.transform; 
+        coin3.transform.parent = CoinParent.transform; 
+    }
+
+    public void RemoveAllCoin()
+    {
+        while(CoinParent.transform.childCount != 0)
+        {
+            GameObject Child = CoinParent.transform.GetChild(0).gameObject;
+            Child.SetActive(false);
+            Child.transform.parent = null;
+        }        
     }
 }
