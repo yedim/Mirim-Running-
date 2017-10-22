ecad318ffef592ab742b53053cce148b3fadf78d
yedim <yelimi0416@gmail.com>
2017-10-23 00:01:11 +0900

    #17 체력바 추가 및 Reset시 체력 회복

---
+++ Running Mirim/Assets/Scripts/HPManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HPManager : MonoBehaviour {
+
+    public Image fillImage;
+    public float timeAmt = 100f;
+    public float time;
+    public bool updateTimer = true;
+
+
+	// Use this for initialization
+	void Start () {
+        time = timeAmt;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(time> 0 && updateTimer==true )
+        {
+            time -= Time.deltaTime;
+            fillImage.fillAmount = time / timeAmt;
+        }      
+	}
+    public void ResetHP()
+    {
+        time = timeAmt;
+        fillImage.fillAmount = 1;
+    }
+
+}
